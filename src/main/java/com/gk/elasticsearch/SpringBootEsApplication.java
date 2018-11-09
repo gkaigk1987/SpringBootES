2a6a1ac8aef1fa517128d1ea39b0f15a1362703c
GaoKai <gkaigk@126.com>
2018-11-09 21:28:38 +0800

    Initial commit

---
+++ src/main/java/com/gk/elasticsearch/SpringBootEsApplication.java
@@ -0,0 +1,12 @@
+package com.gk.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEsApplication.class, args);
+	}
+}
