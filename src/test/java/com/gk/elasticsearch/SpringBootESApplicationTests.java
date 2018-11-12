258c5568cce3ed86ce5d07de69215967fcbbc0c9
GaoKai <gkaigk@126.com>
2018-11-12 21:31:23 +0800

    ElasticsearchTemplate测试
    ElasticsearchTemplate测试

--- src/test/java/com/gk/elasticsearch/SpringBootESApplicationTests.java
+++ src/test/java/com/gk/elasticsearch/SpringBootESApplicationTests.java
@@ -3,10 +3,19 @@ package com.gk.elasticsearch;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.StringQuery;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.gk.elasticsearch.esModel.ESThesis;
@@ -23,6 +32,9 @@ public class SpringBootESApplicationTests {
 	
 	@Autowired
 	private IThesisService thesisService;
+	
+	@Autowired
+	private ElasticsearchTemplate elasticsearchTemplate;
 
 	@Test
 	public void testGetThesisById() {
@@ -61,5 +73,27 @@ public class SpringBootESApplicationTests {
 		esThesis.setTitle("gk_test_thesis_title2");
 		list.add(esThesis);
 		thesisSearchService.addThesisListIndex(list);
+	}
+	
+	@Test
+	public void testTemplate() {
+		//未分页
+//		MatchQueryBuilder matchQuery = QueryBuilders.matchQuery("title", "marketing");
+//		NativeSearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(matchQuery).build();
+//		List<ESThesis> list = elasticsearchTemplate.queryForList(searchQuery, ESThesis.class);
+//		System.out.println(list.size());
+//		list.stream().forEach(e -> {
+//			System.out.println(e.getTitle());
+//		});
+		
+		//分页
+		Pageable pageable = PageRequest.of(0, 20);
+		String source = "{\"match\":{\"title\":\"marketing\"}}";
+		StringQuery sQuery = new StringQuery(source, pageable);
+		Page<ESThesis> queryForPage = elasticsearchTemplate.queryForPage(sQuery, ESThesis.class);
+		System.out.println(queryForPage.getSize());
+		queryForPage.stream().forEach(t -> {
+			System.out.println(t.getTitle());
+		});
 	}
 }
